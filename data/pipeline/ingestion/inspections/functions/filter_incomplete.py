9c83c935dc0649fe6cd485c21f9672276ce5dd58
Daniel Brotz <daniel@brotz.cz>
2023-10-29 15:17:31 +0100

    Fix accidentally dropping inspections during ingestion

--- data/pipeline/ingestion/inspections/functions/filter_incomplete.py
+++ data/pipeline/ingestion/inspections/functions/filter_incomplete.py
@@ -3,22 +3,16 @@ import datetime
 
 
 def filter_incomplete(df: pd.DataFrame, date: datetime.date = None, **kwargs) -> pd.DataFrame:
-    # For 2018, do not drop rows with missing defects (they are all missing)
-    if date.year == 2018:
-        return df.dropna(how='any', subset=[
-            'station_id',
-            'date',
-            'vin',
-            'inspection_type',
-            'result',
-            'mileage',
-            # not 'defects',
-            'make',
-            'motor_type',
-            'vehicle_type',
-            'model_primary',
-            'vehicle_class',
-            'first_registration_date',
-        ])
-    else:
-        return df.dropna(how='any')
+    # Drop inspections with missing data, excl. defects (those are just vehicles
+    # in perfect condition).
+    return df.dropna(how='any', subset=[
+        'station_id',
+        'date',
+        'vin',
+        'inspection_type',
+        'result',
+        'mileage',
+        # not 'defects',
+        'first_registration_date',
+    ])
+    
