3e7df3a9369bbd6ce2709f095d5868ddc40aaa0a
Daniel Brotz <daniel@brotz.cz>
2023-10-29 15:26:05 +0100

    Add vehicle comparison skeleton

--- web/app/src/app/compare/page.tsx
+++ web/app/src/app/compare/page.tsx
@@ -1,46 +1,51 @@
-import Button from "@/components/Button";
-import Card from "@/components/Card";
-import PrimaryButton from "@/components/PrimaryButton";
-import { Rubik } from "next/font/google";
+"use client";
 
-const rubik = Rubik({ subsets: ["latin"] });
+import ConfigProvider from "antd/es/config-provider";
+import theme from "../themeConfig";
+import Layout from "antd/es/layout";
+import Header from "@/components/Header";
+import { Content } from "antd/es/layout/layout";
+import Footer from "@/components/Footer";
+import Button from "antd/es/button";
+import Card from "antd/es/card";
+import Input from "antd/es/input";
 
 export default function ComparePage() {
   return (
-    <main className="flex flex-col items-stretch p-4 space-y-4">
-      <h1 className={`${rubik.className} text-4xl py-2`}>Srovnání vozidel</h1>
+    <ConfigProvider theme={theme}>
+      <Layout>
+        <Header></Header>
+        <Content className="flex flex-col items-stretch p-4 space-y-4">
+          <h1 className="text-3xl">Srovnání vozidel</h1>
 
-      <p>
-        Srovnávač vozidel umožňuje porovnat buď jednotlivá vozidla podle VIN,
-        nebo obecně značky a modely.
-      </p>
+          <p>
+            Srovnávač vozidel umožňuje porovnat buď jednotlivá vozidla podle
+            VIN, nebo obecně značky a modely.
+          </p>
 
-      <div className="flex flex-col space-y-4 items-left">
-        <input
-          type="text"
-          className="p-2 h-12 rounded border-2 outline-none md:w-96 focus:border-red-600 focus:bg-red-50 hover:border-red-600"
-          placeholder="Model, značka nebo VIN"
-          autoFocus
-        ></input>
-        <div className="space-x-4">
-          <input
-            type="text"
-            className="p-2 h-12 rounded border-2 outline-none md:w-96 focus:border-red-600 focus:bg-red-50 hover:border-red-600"
-            placeholder="Model, značka nebo VIN"
-            autoFocus
-          ></input>
-          <Button>Přidat srovnání</Button>
-        </div>
-        <div>
-          <PrimaryButton>Porovnat vozidla</PrimaryButton>
-        </div>
+          <div className="flex flex-col space-y-4 items-left md:w-1/2">
+            <Input placeholder="Model, značka nebo VIN" autoFocus></Input>
+            <Input placeholder="Model, značka nebo VIN"></Input>
 
-        <h2 className={`${rubik.className} text-3xl pt-4`}>Výsledky</h2>
+            <div className="flex justify-center">
+              <Button type="primary">Porovnat vozidla</Button>
+            </div>
+          </div>
+          <div></div>
 
-        <Card>Výsledek 1</Card>
-        <Card>Výsledek 2</Card>
-        <Card>Výsledek 3</Card>
-      </div>
-    </main>
+          <Card>Výsledek 1</Card>
+          <Card>Výsledek 2</Card>
+          <Card>Výsledek 3</Card>
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    </ConfigProvider>
+    // <main className="flex flex-col items-stretch p-4 space-y-4">
+    //   {/* <h1 className={`${rubik.className} text-4xl py-2`}>Srovnání vozidel</h1>
+
+    //
+
+    //   </div> */}
+    // </main>
   );
 }
